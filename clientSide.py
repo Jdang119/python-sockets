cf1ab85bf2c0f4cc0400d3fbb8e7de1752fdac08
Jang Actual <dangjustin95@gmail.com>
2025-02-17 20:57:56 -0600

    Client sending to and receiving from server

--- clientSide.py
+++ clientSide.py
@@ -17,3 +17,17 @@ server_address = ("127.0.0.1", 12345)
 client_socket.connect(server_address)
 print("Connected to the server")
 
+#sending a message to the server
+#converting string to raw bytes before sending using utf-8
+message = "Hello, server!"
+client_socket.send(message.encode("utf-8"))
+
+#receiving a response from the server
+#decoding raw bytes of data from server into readable format
+#setting it to a response variable
+response = client_socket.recv(1024).decode("utf-8")
+print(f"Server response: {response}")
+
+
+#closing connection
+client_socket.close(0)
